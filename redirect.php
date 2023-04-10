436cf37da209f8e5e9c22c961a165ff31713d359
Muhammad Nail Hadi <105335419+muhammadnailhadi@users.noreply.github.com>
2023-04-10 12:24:57 +0700

    Add files via upload

---
+++ redirect.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <meta http-equiv="refresh" content="2; url=http://localhost/Praktikum 4/Selector Id_Praktek 4.html">
+    </head>
+    <body>
+        Tes Redirect
+ </body>
+</html>
